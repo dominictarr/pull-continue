833a35df5d0c639d68f1bfcb75d70b118ce74916
Jan Bölsche <regular.gonzales@googlemail.com>
2016-04-23 12:57:23 +0200

    Fix bug when aborting while read is pending

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
 
 module.exports = function (create) {
-  var i = 0, n = 0, read
+  var i = 0, n = 0, read, aborted
   return function (abort, cb) {
+    if(aborted) return cb(aborted)
+    aborted = abort
     if(abort && !read) return cb(true)
     if(abort)          return read(abort, cb)
     if (!read)         read = create(i++)
 
     read(null, function (end, data) {
-
+      if(aborted) return cb(aborted)
       if(end) {
         read = create(i++, n)
         if(!read) cb(end)
